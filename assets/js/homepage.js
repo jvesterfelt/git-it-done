dcc4de85b4fab1241f6a0d649a2c50182bf7c93b
Jamie Vesterfelt <jamievesterfelt@hotmail.com>
2021-05-28 11:22:07 -0600

    Completed logic for filtering based on language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,17 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+};
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -85,4 +96,20 @@ var getUserRepos = function(user) {
         });
 };
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    displayRepos(data.items, language);
+                });
+            } else {
+                alert("Error: GitHub User Not Found");
+            }
+        });
+};
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
